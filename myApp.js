c7859f01e033b6b49fe159843fc5e3c74b7749ae
Duncanwar <semunda9@gmail.com>
2020-06-01 11:01:56 +0200

    env file

--- myApp.js
+++ myApp.js
@@ -1,52 +1,53 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
+//var env = require("dotenv");
 
 // --> 7)  Mount the Logger middleware here
 
-
 // --> 11)  Mount the body-parser middleware  here
 
-
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+// app.get("/",(req,res)=>{
+//   res.send("Hello Express")
+// })
 
 /** 3) Serve an HTML file */
-
+app.get("/", (req, res) => {
+  res.sendfile(__dirname + "/views/index.html");
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+  const upper = process.env.MESSAGE_STYLE === "uppercase";
+let message = "Hello json";
+if (upper) {
+  message = message.toUpperCase();
+}
+app.get("/json", (req, res) => res.json({
+  "message": message
+}));
 
 /** 6) Use the .env file to configure the app */
- 
- 
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
 
-
 /** 9)  Get input from client - Route parameters */
-
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
-
 /** 12) Get data form POST  */
-
-
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
@@ -54,4 +55,4 @@ var app = express();
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
